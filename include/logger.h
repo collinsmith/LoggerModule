635b609bfc49d445a0f2c509cd77a121eabd4822
Collin Smith <collinsmith70@gmail.com>
2015-09-09 23:54:48 -0700

    Updated logger header to use prototypes instead of directly definining implementation (similar to the design within AMXX). Moved some of these definitions to logger.cpp

--- include/logger.h
+++ include/logger.h
@@ -1,91 +1,63 @@
 #ifndef _LOGGER_H_
-	#define _LOGGER_H_
+#define _LOGGER_H_
 
-	#include <cstdlib>
-	#include <stdio.h>
-	#include <stdarg.h>
-	#include <time.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
 
-	#if defined(_WIN32)
-		#include <io.h>
-	#endif
-	
-	//#include <amxmodx.h>
-	//#include <natives_handles.h>
+#if defined(_WIN32)
+	#include <io.h>
+#endif
 
-	class Logger {
-	private:
-		const char* m_pNameFormat;
-		const char* m_pMessageFormat;
-		const char* m_pDateFormat;
-		const char* m_pTimeFormat;
-		const char* m_pPath;
+#define SEVERITY_ERROR 1000
+#define SEVERITY_WARN 1000
+#define SEVERITY_INFO 1000
+#define SEVERITY_DEBUG 1000
+#define SEVERITY_NONE 1000
 
-		int m_Verbosity;
+//#include <amxmodx.h>
+//#include <natives_handles.h>
 
-	public:
-		Logger(const char* nameFormat, const char* messageFormat, const char* dateFormat, const char* timeFormat, int verbosity, const char* path)
-				: m_pNameFormat(nameFormat), m_pMessageFormat(messageFormat), m_pDateFormat(dateFormat), m_pTimeFormat(timeFormat), m_Verbosity(verbosity), m_pPath(path) {
-		};
+class Logger {
+private:
+	const char* m_pNameFormat;
+	const char* m_pMessageFormat;
+	const char* m_pDateFormat;
+	const char* m_pTimeFormat;
+	const char* m_pPath;
 
-		~Logger() {
-			free((char*)m_pNameFormat);
-			free((char*)m_pMessageFormat);
-			free((char*)m_pDateFormat);
-			free((char*)m_pTimeFormat);
-			free((char*)m_pPath);
-		}
+	unsigned int m_Verbosity;
 
-	public:
-		const char* getNameFormat() const {
-			return m_pNameFormat;
-		};
-
-		const char* getMessageFormat() const {
-			return m_pMessageFormat;
-		};
-
-		const char* getDateFormat() const {
-			return m_pDateFormat;
-		};
-
-		const char* getTimeFormat() const {
-			return m_pTimeFormat;
-		};
-
-		const char* getPath() const {
-			return m_pPath;
-		};
-
-		int getVerbosity() const {
-			return m_Verbosity;
-		};
-
-
-	public:
-		void log(int severity, const char* format, ...) const {
-			if (getVerbosity() <= severity) {
-				return;
-			}
-
-			// get time
-			time_t td;
-			time(&td);
-			tm *curTime = localtime(&td);
-
-			char date[32];
-			strftime(date, 31, "%m/%d/%Y - %H:%M:%S", curTime);
-
-			// msg
-			static char msg[3072];
-
-			va_list arglst;
-			va_start(arglst, format);
-			vsnprintf(msg, 3071, format, arglst);
-			va_end(arglst);
-		}
+public:
+	Logger(const char* nameFormat,
+				const char* messageFormat,
+				const char* dateFormat,
+				const char* timeFormat,
+				unsigned int verbosity,
+				const char* path)
+			: m_pNameFormat(nameFormat),
+				m_pMessageFormat(messageFormat),
+				m_pDateFormat(dateFormat),
+				m_pTimeFormat(timeFormat),
+				m_Verbosity(verbosity),
+				m_pPath(path) {
 	};
 
-	//extern NativeHandle<Logger> LoggerHandles;
+	~Logger() {
+		free((char*)m_pNameFormat);
+		free((char*)m_pMessageFormat);
+		free((char*)m_pDateFormat);
+		free((char*)m_pTimeFormat);
+		free((char*)m_pPath);
+	};
+
+public:
+	unsigned int getVerbosity() const;
+	unsigned int setVerbosity(unsigned int verbosity);
+	void log(int severity, const char* format, ...) const;
+};
+
+//extern NativeHandle<Logger> LoggerHandles;
 
 #endif
--- logger.cpp
+++ logger.cpp
@@ -5,6 +5,43 @@
 
 //NativeHandle<Logger> LoggerHandles;
 
+unsigned int Logger::getVerbosity() const {
+	return m_Verbosity;
+};
+
+unsigned int Logger::setVerbosity(unsigned int verbosity) {
+	int old = m_Verbosity;
+	m_Verbosity = verbosity;
+	return old;
+};
+
+void Logger::log(int severity, const char* format, ...) const {
+	if (m_Verbosity <= severity) {
+		return;
+	}
+
+	// get time
+	time_t td;
+	time(&td);
+	tm *curTime = localtime(&td);
+
+	char date[16];
+	strftime(date, 15, m_pDateFormat, curTime);
+
+	char time[16];
+	strftime(time, 15, m_pTimeFormat, curTime);
+
+	// msg
+	static char msg[3072];
+
+	va_list arglst;
+	va_start(arglst, format);
+	vsnprintf(msg, 3071, format, arglst);
+	va_end(arglst);
+
+	// "[%-5severity] [%time] %message"
+};
+
 void OnAmxxAttach() {
 	//...
 }
@@ -16,3 +53,4 @@ void OnAmxxDetach() {
 static cell AMX_NATIVE_CALL LoggerCreate(AMX* amx, cell* params) {
 	MF_LogError(amx, AMX_ERR_NATIVE, "");
 }
+
